4dddfaeef111504d5322e316275737c467088519
Eric Cornelissen <ericornelissen@gmail.com>
2017-05-13 21:44:56 +0200

    Add bootstrap as dependency

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,30 @@
 
 
 const browserify = require('gulp-browserify');
+const concatCss = require('gulp-concat-css');
 const gulp = require('gulp');
 const rename = require('gulp-rename');
 
 
-gulp.task('build', ['script']);
-gulp.task('default', ['watch-script']);
+gulp.task('build', ['script', 'styles']);
+gulp.task('default', ['watch-script', 'watch-styles']);
 
 
+gulp.task('watch-script', ['script'], () => gulp.watch('public/scripts/*.js', ['script']));
 gulp.task('script', function() {
 	gulp.src('public/scripts/index.js')
 		.pipe(browserify({ insertGlobals: false }))
 		.pipe(rename('script.js'))
 		.pipe(gulp.dest('./public'));
 });
-gulp.task('watch-script', ['script'], function() {
-	gulp.watch('public/scripts/*.js', ['script']);
+
+
+gulp.task('watch-styles', ['styles'], () => gulp.watch('public/styles/*.css', ['styles']));
+gulp.task('styles', function() {
+	gulp.src([
+		'./public/css/*.css',
+		'./node_modules/bootstrap/dist/css/bootstrap.min.css' // Bootstrap
+	])
+		.pipe(concatCss('stylesheet.css', { rebaseUrls: false }))
+		.pipe(gulp.dest('./public'));
 });
