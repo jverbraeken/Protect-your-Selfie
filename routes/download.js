6d34f994a3f9848196715822db7ef678964e57eb
Eric Cornelissen <ericornelissen@gmail.com>
2017-05-14 01:47:17 +0200

    Download as file

--- routes/download.js
+++ routes/download.js
@@ -9,14 +9,13 @@ let router = express.Router();
 router.get('/download/:file', function(req, res) {
 	aws.download(req.params.file)
 		.then(file => {
-			res.setHeader('Content-disposition', 'attachment; filename=download.txt');
+			res.setHeader('Content-disposition', 'attachment; filename=' + req.params.file);
 			res.setHeader('Content-type', 'text/plain');
 			res.charset = 'UTF-8';
-			res.write(file).end();
+			res.write(file);
+			res.end();
 		})
-		.catch(e => {
-			res.status(418).end('Failed!');
-		});
+		.catch(e => res.status(418).end('Failed!'));
 });
 
 
--- routes/upload.js
+++ routes/upload.js
@@ -10,9 +10,7 @@ router.get('/upload/:file/:data', function(req, res) {
 	aws.upload(req.params.file, req.params.data)
 		.then(url => console.log(url))
 		.then(() => res.status(200).end('Success!'))
-		.catch(e => {
-			res.status(418).end('Failed!');
-		});
+		.catch(e => res.status(418).end('Failed!'));
 });
 
 
