ce34e6c7af1a01864378961c2ef8cff3febb86a3
Eric Cornelissen <ericornelissen@gmail.com>
2017-05-14 01:17:00 +0200

    Download, show content of file on page

--- routes/download.js
+++ routes/download.js
@@ -8,8 +8,11 @@ let router = express.Router();
 
 router.get('/download/:file', function(req, res) {
 	aws.download(req.params.file)
-		.then(url => console.log(url))
-		.then(() => res.status(200).end('Success!'))
+		.then(file => {
+			console.log(file);
+			return file;
+		})
+		.then(file => res.status(200).send(file))
 		.catch(e => {
 			res.status(418).end('Failed!');
 		});
