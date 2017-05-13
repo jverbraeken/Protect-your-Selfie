a6f491c65dc1fc670df786c6b38d6d6dc6b9e758
Joost Verbraeken <mail@joostverbraeken.com>
2017-05-13 23:51:37 +0200

    Add final database'

--- db_init.sql
+++ db_init.sql
@@ -3,3 +3,15 @@ CREATE TABLE users (
 	username			varchar(80),
 	password			varchar(80)
 );
+
+CREATE TABLE files (
+	id					serial PRIMARY KEY,
+	amazon_path			varchar(1024)
+);
+
+CREATE TABLE relations (
+	file_owner			serial references users(id),
+	granted_user		serial references users(id),
+	encrypted_key		varchar(256),
+	associated_file		serial references files(id)
+)
