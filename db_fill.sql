323b33c2d39ae12d92e21bb23e4ba9846549de40
Eric Cornelissen <ericornelissen@gmail.com>
2017-05-14 12:33:44 +0200

    db fill

--- db_fill.sql
+++ db_fill.sql
@@ -1,1 +1,2 @@
-INSERT INTO users(username, password) VALUES ('eric', 'eric');
+INSERT INTO users(name, username, password, is_organization) VALUES ('Cornel de Vroomen', 'user', 'user', false);
+INSERT INTO users(name, username, password, is_organization) VALUES ('Wouter', 'aegon', 'aegon', true);
