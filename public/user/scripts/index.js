d9a35075dee8e198f7f7d9a33b6d877d3724ab84
Cornel <c.o.devroomen@student.tudelft.nl>
2017-05-14 04:20:01 +0200

    Two seperate directories

---
+++ public/user/scripts/index.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const Vue = require('vue');
+
+document.addEventListener('DOMContentLoaded', function() {
+	var app = new Vue({
+		el: '#app',
+		data: {
+			title: 'Hello Vue!',
+			text: 'Lorem ipsum dolor'
+		}
+	});
+});
