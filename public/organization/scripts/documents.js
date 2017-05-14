67312496c931b8158ba601c46ae0bcd43259927f
Cornel <c.o.devroomen@student.tudelft.nl>
2017-05-14 07:52:32 +0200

    sdf

--- public/organization/scripts/documents.js
+++ public/organization/scripts/documents.js
@@ -4,26 +4,22 @@ const Vue = require('vue');
 const request = require('request');
 const request_promise = require('request-promise');
 
-document.addEventListener('DOMContentLoaded', function() {
-	var app = new Vue({
-		el: '#app',
-		data: {
-			title: 'Hello Vue!',
-			text: 'Lorem ipsum dolor',
-			filename: 'filename'
-		},
-		methods: {
-			showPopup: function() {
-
+var app = new Vue({
+	el: '#documentListTable',
+	data: {
+		documents: [
+			{
+				name: "Payslip 24-02-17",
+				filename: "payslip.pdf",
+				description: "Payslip of the second month of 2017",
+				uploadDate: "clientProfile.html",
+				access: ["Aegon", "Het Spaarne ziekenhuis"]
 			}
-		}
-	});
-
-	console.log(window.url);
-	app.showPopup();
+		]
+	}
 });
 
 let table = document.getElementById("table_documents");
-request.get("http://localhost:7000/get_files").then(function(tmp) {
+request.get("/get_files").then(function(tmp) {
 	console.log(tmp);
 });
